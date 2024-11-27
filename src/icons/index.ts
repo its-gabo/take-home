16e80020cf0d85ea70e8b5402a932b22c5a5f541
Gabo <smolana.gabriel@gmail.com>
2024-11-27 12:01:57 +0100

    refactor: move icons to /icons folder and separate them into files

---
+++ src/icons/index.ts
@@ -0,0 +1,6 @@
+import { ChevronUpIcon } from "./ChevronUpIcon";
+import { ChevronDownIcon } from "./ChevronDownIcon";
+import { XMarkIcon } from "./XMarkIcon";
+import { RevertIcon } from "./RevertIcon";
+
+export { ChevronUpIcon, ChevronDownIcon, XMarkIcon, RevertIcon };
